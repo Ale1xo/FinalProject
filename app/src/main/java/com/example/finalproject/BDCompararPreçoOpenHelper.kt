8c377914ec6010219001739b324e5adbc0ac0718
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-07 23:09:25 +0100

    BDCompararPrecoOpenHelper criada.

--- app/src/main/java/com/example/finalproject/BDCompararPreçoOpenHelper.kt
+++ app/src/main/java/com/example/finalproject/BDCompararPreçoOpenHelper.kt
@@ -1,4 +1,25 @@
 package com.example.finalproject
 
-class BDCompararPreçoOpenHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BDCompararPreçoOpenHelper(context : Context?) : SQLiteOpenHelper(context, NOME, null, VERSAO ) {
+
+    override fun onCreate(db : SQLiteDatabase){
+        requireNotNull(db)
+        TableAvaliacao(db).cria()
+        TableProdutos(db).cria()
+        TableLoja(db).cria()
+        TableMarcas(db).cria()
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val NOME = "CompararPreco.db"
+        private const val VERSAO = 1
+    }
 }
