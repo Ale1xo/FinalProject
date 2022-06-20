bb2d8a729910d5319e1a371b0b9f5711e30d2521
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-20 21:44:54 +0100

    Tabela TableAvaliação atualizada

--- app/src/main/java/com/example/finalproject/TableAvaliacao.kt
+++ app/src/main/java/com/example/finalproject/TableAvaliacao.kt
@@ -7,20 +7,15 @@ class TableAvaliacao(db : SQLiteDatabase) : TabelaBD(db, NAME) {
     override fun cria() {
         db.execSQL("CREATE TABLE $nome(${BaseColumns._ID}INTEGER PRIMARY KEY AUTOINCREMIDLO" +
                 "$IDAVALIACAO INTEGER NOT NULL," +
-                "$NAME TEXT NOT NULL)" +
-                "$AVALIACAO TEXT NOT NULL)" +
-                "$PRODUTO_ID INTEGER NOT NULL)" +
-                "$LOJA_ID INTEGER NOT NULL)" +
-                "FOREIGN KEY($PRODUTO_ID)" +
-                "FOREIGN KEY($LOJA_ID)" +
-                "REFERENCES ${TableProdutos.NAME}(${BaseColumns._ID}, ${TableLoja.NAME}(${BaseColumns._ID})")
+                "$AVALIACAO INTEGER NOT NULL)" +
+                "FOREIGN KEY(${PRODUTO_ID}" +
+                "REFERENCES ${TableProdutos.NAME}(${BaseColumns._ID}")
     }
 
     companion object{
-        const val  IDAVALIACAO = "Id"
-        const val NAME = "Avalicao"
+        const val IDAVALIACAO = "Id"
+        const val NAME = "Name"
         const val AVALIACAO = "Qualificacao"
         const val PRODUTO_ID = "ProdutoId"
-        const val LOJA_ID = "LojaId"
     }
 }
