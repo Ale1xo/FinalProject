dc425052dc55bf96a2068301da8e852baa57f242
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-20 21:44:14 +0100

    Tabela Avaliação atualizada

--- app/src/main/java/com/example/finalproject/Avaliacao.kt
+++ app/src/main/java/com/example/finalproject/Avaliacao.kt
@@ -1,13 +1,29 @@
 package com.example.finalproject
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
-class Avaliacao (var idAvaliacao: Long, var name: String, var avaliacao: Long){
+class Avaliacao (var idAvaliacao: Long = -1, var avaliacao: Long, var idProduto: Long ){
     fun toContentValues(): ContentValues {
         val valoresAvaliacao = ContentValues()
-        valoresAvaliacao.put(TableAvaliacao.NAME, name)
-        valoresAvaliacao.put(TableAvaliacao.IDAVALIACAO, idAvaliacao)
         valoresAvaliacao.put(TableAvaliacao.AVALIACAO, avaliacao)
+        valoresAvaliacao.put(TableAvaliacao.PRODUTO_ID, idProduto)
         return valoresAvaliacao
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Avaliacao{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posAvaliacao = cursor.getColumnIndex(TableAvaliacao.IDAVALIACAO)
+            val posIdProduto = cursor.getColumnIndex(TableAvaliacao.PRODUTO_ID)
+
+            val id = cursor.getLong(posId)
+            val avaliacao = cursor.getLong(posAvaliacao)
+            val idProduto = cursor.getLong(posIdProduto)
+
+            return Avaliacao(id,avaliacao,idProduto)
+
+        }
     }
 }
