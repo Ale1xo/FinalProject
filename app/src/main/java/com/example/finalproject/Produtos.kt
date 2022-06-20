774ebfc6914927f4cd031d697f597a43e6003cc3
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-20 22:32:42 +0100

    Tabela Produtos atualizada

--- app/src/main/java/com/example/finalproject/Produtos.kt
+++ app/src/main/java/com/example/finalproject/Produtos.kt
@@ -1,13 +1,32 @@
 package com.example.finalproject
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
-class Produtos (var idMarca: Long, var name: String, var preco : Long){
+class Produtos (var idProdutos: Long = -1, var name: String, var preco : Long, var idMarca : Long ){
     fun toContentValues(): ContentValues {
         val valoresProdutos = ContentValues()
         valoresProdutos.put(TableProdutos.NAME, name)
-        valoresProdutos.put(TableProdutos.IDMARCA, idMarca)
         valoresProdutos.put(TableProdutos.PRECO, preco)
+        valoresProdutos.put(TableProdutos.IDMARCA, idMarca)
         return valoresProdutos
     }
+
+    companion object{
+        fun fromCursor(cursor: Cursor):Produtos{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posName = cursor.getColumnIndex(TableProdutos.NAME)
+            val posPreco = cursor.getColumnIndex(TableProdutos.PRECO)
+            val posidMarca = cursor.getColumnIndex(TableProdutos.IDMARCA)
+
+            val id = cursor.getLong(posId)
+            val name = cursor.getString(posName)
+            val preco = cursor.getLong(posPreco)
+            val idMarca = cursor.getLong(posidMarca)
+
+            return Produtos(id,name,preco,idMarca)
+        }
+    }
+
 }
