df5ff0e2223b20ab33f75722be3441ba9ff82e57
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-20 20:15:11 +0100

    Tabela Loja atualizada

--- app/src/main/java/com/example/finalproject/Loja.kt
+++ app/src/main/java/com/example/finalproject/Loja.kt
@@ -1,14 +1,29 @@
 package com.example.finalproject
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
-class Loja(var idLoja: Long, var name : String, var localizacao: String) {
+class Loja(var idLoja: Long = -1, var name : String, var localizacao: String) {
 
     fun toContentValues(): ContentValues{
         val valoresLoja = ContentValues()
         valoresLoja.put(TableLoja.NAME, name)
-        valoresLoja.put(TableLoja.IDLOJA, idLoja)
         valoresLoja.put(TableLoja.LOCALIZACAO, localizacao)
         return valoresLoja
+    }
+
+    companion object{
+        fun fromCursor(cursor:Cursor): Loja{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posName = cursor.getColumnIndex(TableLoja.NAME)
+            val posLocalizacao = cursor.getColumnIndex(TableLoja.LOCALIZACAO)
+
+            val id = cursor.getLong(posId)
+            val name = cursor.getString(posName)
+            val localizacao = cursor.getString(posLocalizacao)
+
+            return Loja(id,name,localizacao)
+        }
     }
 }
