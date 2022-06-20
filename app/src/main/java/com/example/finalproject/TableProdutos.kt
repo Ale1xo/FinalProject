d677da2b30f2228e50be5153d33ce88300f8d454
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-20 23:10:31 +0100

    Tabela TableProdutos atualizada

--- app/src/main/java/com/example/finalproject/TableProdutos.kt
+++ app/src/main/java/com/example/finalproject/TableProdutos.kt
@@ -1,23 +1,41 @@
 package com.example.finalproject
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteQueryBuilder
 import android.provider.BaseColumns
 
 class TableProdutos(db : SQLiteDatabase) : TabelaBD(db, NAME) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $nome(${BaseColumns._ID}INTEGER PRIMARY KEY AUTOINCREMIDLO" +
-                "$IDPRODUTO INTEGER NOT NULL," +
-                "$NAME TEXT NOT NULL," +
+        db.execSQL("CREATE TABLE $nome(${BaseColumns._ID}INTEGER PRIMARY KEY AUTOINCRCAMPO_" +
+                "$CAMPO_NAME TEXT NOT NULL," +
                 "$IDMARCA INTEGER NOT NULL," +
                 "$PRECO REAL NOT NULL," +
-                "FOREIGN KEY ($IDMARCA)), " +
-                "REFERENCES ${TableMarcas.NAME}(${BaseColumns._ID}) ")
+                "FOREIGN KEY ($IDMARCA), " +
+                "REFERENCES ${TableMarcas.NAME}(${BaseColumns._ID})ON DELETE RESTRICT) ")
+    }
+    override fun query(
+        columns: Array<String>,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        groupBy: String?,
+        having: String?,
+        orderBy: String?
+    ): Cursor{
+        val queryBuilder = SQLiteQueryBuilder()
+        queryBuilder.tables = "$NAME INNER JOIN ${TableMarcas.NAME} ON ${TableMarcas.IDMARCA} = $IDMARCA"
+
+        return  queryBuilder.query(db,columns,selection,selectionArgs,groupBy,having,orderBy)
     }
 
     companion object{
-        const val  IDPRODUTO = "Id"
-        const val NAME = "Produto"
+        const val NAME = "Produtos"
+
+        const val IDPRODUTO = "$NAME.${BaseColumns._ID}"
+        const val CAMPO_NAME = "Nome"
         const val IDMARCA = "MarcaId"
         const val PRECO = "Valor"
+
+        val  TODAS_COLUNAS = arrayOf(IDPRODUTO, CAMPO_NAME, IDMARCA, PRECO)
     }
 }
