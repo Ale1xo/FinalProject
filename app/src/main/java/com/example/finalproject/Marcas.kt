60b0cf57dce3366f2ebb9e9036da981688aea5d5
Ale1xo <joaoaleixo2014@hotmail.com>
2022-06-20 23:53:13 +0100

    Tabela Marcas atualizada

--- app/src/main/java/com/example/finalproject/Marcas.kt
+++ app/src/main/java/com/example/finalproject/Marcas.kt
@@ -1,12 +1,25 @@
 package com.example.finalproject
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
-class Marcas(var idMarca : Long, var name : String) {
+class Marcas(var id : Long = -1, var name : String) {
     fun toContentValues(): ContentValues {
         val valoresMarca = ContentValues()
         valoresMarca.put(TableLoja.NAME, name)
-        valoresMarca.put(TableLoja.IDLOJA, idMarca)
+
         return valoresMarca
+    }
+    companion object{
+        fun fromCursor( cursor: Cursor): Marcas{
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posName = cursor.getColumnIndex(TableMarcas.NAME)
+
+            val id = cursor.getLong(posId)
+            val name = cursor.getString(posName)
+
+            return Marcas(id,name)
+        }
     }
 }
