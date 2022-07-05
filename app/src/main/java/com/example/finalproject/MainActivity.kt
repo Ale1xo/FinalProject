f18af88023ac60828bded9fe4469b265469ada0c
Ale1xo <joaoaleixo2014@hotmail.com>
2022-07-05 17:16:46 +0100

    Função MainActivity criada

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -9,12 +9,25 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.example.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    var idMenuAtual = R.menu.menu_main
+        get() = field
+        set(value) {
+            if(value != field){
+                field = value
+                invalidateOptionsMenu()
+            }
+        }
+    var menu: Menu?= null
+    var fragment : Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +41,33 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+
+        menuInflater.inflate(idMenuAtual, menu)
+        this.menu = menu
+        return  true
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val opcaoProcessada: Boolean
+
+        if(fragment is MenuInicialFragment){
+            opcaoProcessada = (fragment as MenuInicialFragment).processaOpcaoMenu(item)
+        }else if(fragment is ListaProdutosFragment){
+            opcaoProcessada = (fragment as ListaProdutosFragment).processaOpcaoMenu(item)
+        }else{
+            opcaoProcessada = false
         }
+
+        if(opcaoProcessada) return  true
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
