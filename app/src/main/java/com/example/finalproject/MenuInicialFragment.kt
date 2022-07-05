b0a511e614528eacc4598d689903c086b0f479ab
Ale1xo <joaoaleixo2014@hotmail.com>
2022-07-05 17:21:34 +0100

    MenuInicialFragment atualizada

--- app/src/main/java/com/example/finalproject/MenuInicialFragment.kt
+++ app/src/main/java/com/example/finalproject/MenuInicialFragment.kt
@@ -3,17 +3,18 @@ package com.example.finalproject
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.finalproject.databinding.FragmentFirstBinding
+import com.example.finalproject.databinding.FragmentMenuInicialBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MenuInicialFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentMenuInicialBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentMenuInicialBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,13 +33,24 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.buttonProduto.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_main
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-}
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean =
+        when(item.itemId){
+            R.id.action_settings -> true
+            else -> false
+
+        }        }
+
